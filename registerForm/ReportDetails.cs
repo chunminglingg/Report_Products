99a856e41129880035cc5aa5d9e7b5e7052b9e77
Chunming <chunming200@gmail.com>
2025-01-03 14:12:17 +0700

    Report

---
+++ registerForm/ReportDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace registerForm
+{
+    internal class ReportDetails
+    {
+        public int No {  get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Qty { get; set; }
+        public double Amount { get { return Price * Qty; } }
+        public ReportDetails() 
+        {
+
+        }
+      
+
+
+    }
+}
